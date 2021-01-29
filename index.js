f0fc4c0cb9beffac4cdbf85235b52cf505dc5559
ArcT5555 <65627943+ArcT5555@users.noreply.github.com>
2021-01-29 10:45:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
@@ -10,5 +11,10 @@ client.on("message", (msg) => {
         msg.reply("General Kenobi, it is pleasning to hear you say that!")
     }
 })
+client.on("message", (msg) => {
+    if (msg.content === "Thrawn. Have you had your coffee?") {
+        msg.reply("Yes Sir. I'll be awake all day, 24/7")
+    }
+})
 
-client.login(process.env.token);
+client.login(process.env.token)
